cb7a924925d59e4da72669b9422be9d3ff89cd7d
Fabian Lindner <fabian@FabiansArch.localdomain>
2020-09-21 21:10:27 +0200

    always center

--- config.h
+++ config.h
@@ -2,8 +2,8 @@
 /* Default settings; can be overriden by command line. */
 
 static int topbar = 1;                      /* -b  option; if 0, dmenu appears at bottom     */
-static int centered = 0;                    /* -c option; centers dmenu on screen */
-static int min_width = 500;                    /* minimum width when centered */
+static int centered = 1;                    /* -c option; centers dmenu on screen */
+static int min_width = 0;                    /* minimum width when centered */
 static int animated = 1;
 static int framecount = 8;
 /* -fn option overrides fonts[0]; default X11 font or font set */
@@ -21,7 +21,7 @@ static char *colors[SchemeLast][2] = {
 	[SchemeBorder] = { "#000000" /* ignored */, "#005577" },
 };
 /* -l option; if nonzero, dmenu uses vertical list with given number of lines */
-static unsigned int lines      = 0;
+static unsigned int lines      = 5;
 
 /*
  * Characters not considered part of a word while deleting words
